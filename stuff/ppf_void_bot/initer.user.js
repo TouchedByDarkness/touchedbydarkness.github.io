ea6b83fd62f47a5cb20922cc118d00622cd2bbd5
TouchedByDarkness <53915513+TouchedByDarkness@users.noreply.github.com>
2022-12-11 13:08:16 +0500

    Create initer.user.js

---
+++ stuff/ppf_void_bot/initer.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         PP Void Bot
+// @version      1.0
+// @description  Make us great again!
+// @author       Darkness
+// @grant 		 GM_xmlhttpRequest
+// @grant 		 unsafeWindow
+// @require		 https://raw.githubusercontent.com/mitchellmebane/GM_fetch/master/GM_fetch.min.js
+// @require		 https://raw.githubusercontent.com/TouchedByDarkness/PixelPlanet-Void-Bot/master/base64.js
+// @connect		 githubusercontent.com
+// @connect		 github.com
+// @connect		 glitch.me
+// @connect		 pixelplanet.fun
+// @connect		 fuckyouarkeros.fun
+// @downloadURL  https://touchedbydarkness.github.io/stuff/ppf_bot/initer.user.js
+// @updateURL  	 https://touchedbydarkness.github.io/stuff/ppf_bot/initer.user.js
+// @match      *://fuckyouarkeros.fun/*
+// @match      *://pixelplanet.fun/*
+// ==/UserScript==
+
+fetch('https://raw.githubusercontent.com/TouchedByDarkness/PixelPlanet-Void-Bot/master/bytecode')
+.then(res => {
+	if (res.readyState !== res.DONE) {
+		return;
+	}
+
+	if (res.status !== 200) {
+		alert(`cant load Void bot\ncode: ${res.status}\nreason: ${res.statusText}`);
+		return;
+	}
+
+	return res.text()
+})
+.then(bytecode => {
+	if (bytecode !== undefined) {
+		new Function("const [self, GM, unsafeWindow] = arguments; " + atob(bytecode))(self, GM, unsafeWindow);
+	}
+});
